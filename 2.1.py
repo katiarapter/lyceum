60339cd79fd98994b16697e2b3eb397dd3b7f067
Katiapater <katiasafronova08@yandex.ru>
2022-11-24 23:10:37 +0300

    удаление

--- 2.1.py
+++
@@ -1,8 +0,0 @@
-a = input()
-# надо вввести строку, где числа надо вводить через пробел
-spisok = a.split()
-for elem in spisok:
-    if elem == '237':
-        break
-    if int(elem) % 2 == 0:
-        print(elem)
--- pythonProject — копия/задачи.py
+++
@@ -1,9 +0,0 @@
-size = int(input())
-matrix = [[0] * size for _ in range(size)]
-for i in range(1, size):
-    st = input().split()
-    for col in range(len(st)):
-        matrix[i][col] = matrix[col][i] = int(st[col])
-table = matrix
-for elem in table:
-    print(*elem)
