f3ddc4cde79910a09a1261ec37e8f1ded58cdb45
Katiapater <katiasafronova08@yandex.ru>
2022-11-25 21:34:08 +0300

    connect 1 ui file

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Hello World')
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('first.ui', self)
+        self.pushButton.clicked.connect(self.menager)
+        self.pushButton_2.clicked.connect(self.assistant)
+        self.pushButton_3.clicked.connect(self.visitor)
+
+    def menager(self):
+        pass
+
+    def assistant(self):
+        pass
+
+    def visitor(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
